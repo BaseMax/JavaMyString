466a66d6f3a87ae1ee4aa54d130a7a062c5051dc
Max Base <Max@Asrez.com>
2019-12-04 15:01:33 +0330

    Add some new features...

--- com/asrez/utils/MyString.java
+++ com/asrez/utils/MyString.java
@@ -48,6 +48,268 @@ public class MyString {
 			array[i] = value.charAt(i);
 	}
 
+	public int indexOf(int ch, int fromIndex) {
+		if(fromIndex < 0)
+			fromIndex = 0;
+		else if(fromIndex >= length)
+			return -1;
+
+		for(int i = fromIndex; i < length; i++)
+			if(array[i] == ch)
+				return i;
+		return -1;
+	}
+
+	public int indexOf(int ch) {
+		return indexOf(ch, 0);
+	}
+
+	public int indexOf(MyString str) {
+		return indexOf(str, 0);
+	}
+
+	public int indexOf(MyString str, int fromIndex) {
+		if(fromIndex < 0)
+			fromIndex = 0;
+		else if(fromIndex >= length)
+			return -1;
+
+		for(int i = fromIndex; i < length; i++) {
+			if(array[i] == str.charAt(0)) {
+				if(str.length() == 1)
+					return i;
+				int offset = 1; 
+				while(offset < str.length()
+					  &&
+					 ((i + offset) < length)
+					  &&
+					 ((str.charAt(offset)) ==(array[i + offset])))
+				{
+					if(offset == str.length() - 1)
+						return i;
+					offset++;
+				}
+			}
+		}
+		return -1;
+	}
+
+	public int indexOf(String str) {
+		return indexOf(str, 0);
+	}
+
+	public int indexOf(String str, int fromIndex) {
+		if(fromIndex < 0)
+			fromIndex = 0;
+		else if(fromIndex >= length)
+			return -1;
+
+		for(int i = fromIndex; i < length; i++)
+		{
+			if(array[i] == str.charAt(0)) {
+				if(str.length() == 1)
+					return i;
+				int offset = 1; 
+				while(offset < str.length()
+					  &&
+					 ((i + offset) < length)
+					  &&
+					 ((str.charAt(offset)) ==(array[i + offset])))
+				{
+					if(offset == str.length() - 1)
+						return i;
+					offset++;
+				}
+			}
+		}
+		return -1;
+	}
+
+	public int lastIndexOf(int ch) {
+		return lastIndexOf(ch, length - 1);
+	}
+
+	public int lastIndexOf(int ch, int fromIndex) {
+		
+		if(fromIndex >= length)
+			fromIndex = length -1;
+		
+		for(int i = fromIndex; i >= 0; i--)
+			if(array[i] == ch)
+				return i;
+		return -1;
+	}
+
+	public int lastIndexOf(String str) {
+		return lastIndexOf(str, length - 1);
+	}
+
+	public int lastIndexOf(String str, int fromIndex) {
+		if(fromIndex >= length)
+			fromIndex = length -1;
+
+		for(int i = fromIndex ; i >= 0; i--)
+		{
+			if(array[i] == str.charAt(0)) {
+				if(str.length() == 1)
+					return i;
+				int offset = 1; 
+				while(offset < str.length()
+					  &&
+					 ((i + offset) < length)
+					  &&
+					 ((str.charAt(offset)) ==(array[i + offset]))) {
+					if(offset == str.length() - 1)
+						return i;
+					offset++;
+				}
+			}
+		}
+		return -1;
+	}
+
+
+	public boolean equalsIgnoreCase(MyString other) {
+		if(this.length != other.length)
+			return false;
+			
+		int i = 0;
+		char thisTemp,otherTemp;
+		 
+		while(i < this.length) {
+			if(this.array[i] >= 'A' && this.array[i] <= 'Z')
+				thisTemp =(char)(this.array[i] - 'A' + 'a');
+			else
+				thisTemp = this.array[i];
+				
+			if(other.array[i] >= 'A' && other.array[i] <= 'Z')
+				otherTemp =(char)(other.array[i] - 'A' + 'a');
+			else
+				otherTemp = other.array[i];
+		
+			if(thisTemp != otherTemp)
+				return false;
+				
+			i++;
+		}
+		
+		return true;		
+	}
+
+	public boolean equalsIgnoreCase(String other) {
+		if(this.length != other.length())
+			return false;
+			
+		int i = 0;
+		char thisTemp,otherTemp;
+		 
+		while(i < this.length) {
+			if(this.array[i] >= 'A' && this.array[i] <= 'Z')
+				thisTemp =(char)(this.array[i] - 'A' + 'a');
+			else
+				thisTemp = this.array[i];
+				
+			if(other.charAt(i) >= 'A' && other.charAt(i) <= 'Z')
+				otherTemp =(char)(other.charAt(i) - 'A' + 'a');
+			else
+				otherTemp = other.charAt(i);
+		
+				
+			if(thisTemp != otherTemp)
+				return false;
+				
+			i++;
+		}
+		
+		return true;		
+	}
+
+	public MyString toLowerCase() {
+		MyString temp = new MyString(); 
+		temp.length = this.length;
+		temp.array = new char [this.length];
+		
+		for(int i = 0; i < this.length; i++)
+		{
+			if((this.array[i] >= 'A') &&(this.array[i] <= 'Z'))
+				temp.array[i] =(char)(this.array[i] + 32);
+			else
+				temp.array[i] = this.array[i];
+		}
+		return temp;
+	}
+
+	public MyString toUpperCase() {
+		MyString temp = new MyString(); 
+		temp.length = this.length;
+		temp.array = new char [this.length];
+		
+		for(int i = 0; i < this.length; i++)
+		{
+			if((this.array[i] >= 'a') &&(this.array[i] <= 'z'))
+				temp.array[i] =(char)(this.array[i] - 32);
+			else
+				temp.array[i] = this.array[i];
+		}
+		return temp;
+	}
+
+	public int compareTo(MyString value) {
+		if(this.length != value.length)
+			return this.length - value.length;
+
+		for(int i = 0; i < length; i++)
+			if(this.array[i] != value.array[i])
+				return this.array[i] - value.array[i];
+		
+		return 0;
+	}
+
+	public int compareToIgnoreCase(MyString value) {
+		return this.toLowerCase().compareTo(value.toLowerCase());		
+	}
+
+	public MyString trim() {
+		int frontSpaces = 0, backSpaces = 0;
+		int i = 0;
+		while((i < length) &&(array[i++] == ' ')) 
+			frontSpaces++; 
+		
+		i = length - 1;
+		while((i >= frontSpaces) &&(array[i--] == ' '))  
+			backSpaces++;
+		
+		MyString result = new MyString();
+		
+		if((frontSpaces + backSpaces) <= length)
+		{
+			result.length = this.length - frontSpaces - backSpaces;
+			result.array = new char [result.length];
+			for(i = 0; i < result.length; i++)
+				result.array[i] = this.array[i + frontSpaces];
+		}
+		return result;
+	}
+
+
+	public static MyString valueOf(boolean b) {
+		if(b) 
+			return new MyString("true");
+		else
+			return new MyString("false");
+	}
+
+	public static String valueOf(Object obj) {
+		if(obj == null)
+			return "null";
+		return obj.toString();
+	}
+
+	public String toString() {
+		return new String(array);
+	}
+
+
 	public boolean equals(Object o) {
 		MyString other;
 		if(!(o instanceof MyString))
@@ -68,18 +330,18 @@ public class MyString {
 		return true;
 	}
 
-	static public void printf(char[] format, Object... arguments) {
+	public static void printf(char[] format, Object... arguments) {
 		MyString mystr=new MyString(format);
 		printf(mystr, arguments);
 	}
 
-	static public void printf(String format, Object... arguments) {
+	public static void printf(String format, Object... arguments) {
 		char[] formats=format.toCharArray();
 		MyString mystr=new MyString(formats);
 		printf(mystr, arguments);
 	}
 
-	static public void printf(MyString format, Object... arguments) {
+	public static void printf(MyString format, Object... arguments) {
 		int argumentIndex=0;
 		for(int i = 0; i < format.array.length; i++) {
 			if(format.array[i] == '%') {
@@ -95,7 +357,7 @@ public class MyString {
 								throw new StringIndexOutOfBoundsException(argumentIndex);
 							try {
 								System.out.print((Integer) arguments[argumentIndex]);
-							} catch (ArrayIndexOutOfBoundsException exception) {
+							}catch (ArrayIndexOutOfBoundsException exception) {
 								throw new StringIndexOutOfBoundsException(argumentIndex);
 							}
 							argumentIndex++;
@@ -106,7 +368,7 @@ public class MyString {
 								throw new StringIndexOutOfBoundsException(argumentIndex);
 							try {
 								System.out.print((Double) arguments[argumentIndex]);
-							} catch (ArrayIndexOutOfBoundsException exception) {
+							}catch (ArrayIndexOutOfBoundsException exception) {
 								throw new StringIndexOutOfBoundsException(argumentIndex);
 							}
 							argumentIndex++;
@@ -117,7 +379,7 @@ public class MyString {
 								throw new StringIndexOutOfBoundsException(argumentIndex);
 							try {
 								System.out.print((Long) arguments[argumentIndex]);
-							} catch (ArrayIndexOutOfBoundsException exception) {
+							}catch (ArrayIndexOutOfBoundsException exception) {
 								throw new StringIndexOutOfBoundsException(argumentIndex);
 							}
 							argumentIndex++;
@@ -128,7 +390,7 @@ public class MyString {
 								throw new StringIndexOutOfBoundsException(argumentIndex);
 							try {
 								System.out.print((char) arguments[argumentIndex]);
-							} catch (ArrayIndexOutOfBoundsException exception) {
+							}catch (ArrayIndexOutOfBoundsException exception) {
 								throw new StringIndexOutOfBoundsException(argumentIndex);
 							}
 							argumentIndex++;
@@ -139,7 +401,7 @@ public class MyString {
 								throw new StringIndexOutOfBoundsException(argumentIndex);
 							try {
 								System.out.print((String) arguments[argumentIndex]);
-							} catch (ArrayIndexOutOfBoundsException exception) {
+							}catch (ArrayIndexOutOfBoundsException exception) {
 								throw new StringIndexOutOfBoundsException(argumentIndex);
 							}
 							argumentIndex++;
@@ -175,12 +437,11 @@ public class MyString {
 	}
 
 	public void display() {
-
 		System.out.println(array);
 		// for(int i = 0; i < this.array.length; i++) {
 		// 	if(array[i] == '\n') {
 		// 		break;
-		// 	} else {
+		// 	}else {
 		// 		System.out.format("%c", array[i]);
 		// 	}
 		// }
@@ -200,10 +461,6 @@ public class MyString {
 		return -1;
 	}
 
-	/**
-	 * Returns the length of string
-	 * @return
-	 */
 	public int length() {
 		return length;
 	}
